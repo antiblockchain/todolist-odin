a0e4b5d8b8968a1a06ced3b42526e9ac37a1b53d
antiblockchain <57572382+antiblockchain@users.noreply.github.com>
2023-03-30 08:38:46 -0500

    Added local storage and debugging options

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ function pushItem(currentItem) {
     currentList.appendChild(currentDesc);
     currentList.appendChild(currentButton);
     currentDesc.innerHTML = currentItem.desc.toString();
-    currentButton.innerHTML = "Remove";
+    currentButton.innerHTML = "X";
     currentButton.setAttribute("onclick", "removeItem(" + currentItem.id.toString() + ")");
 
 
@@ -54,3 +54,44 @@ function storeItems() {
 
 }
 
+setInterval(function() {
+    save();
+}, 15000);
+
+function save() {
+    let saveSlot = {
+        counter: counter
+    };
+    localStorage.setItem("saveSlot", JSON.stringify(saveSlot));
+    localStorage.setItem("itemsList", JSON.stringify(items))
+}
+
+document.addEventListener("keydown", function(addEventListener) {
+    if (event.ctrlKey && event.which == 83) {
+        event.preventDefault();
+        save();
+        }
+}, false);
+
+function load() {
+    let currentSave = JSON.parse(localStorage.getItem("saveSlot"));
+    let currentItems = JSON.parse(localStorage.getItem("itemsList"));
+    if (typeof currentItems !== "undefined") items = currentItems;
+    if (typeof currentSave.counter !== "undefined") counter = currentSave.counter;
+    if (counter > 0) {
+        for (let i = 0; i < currentItems.length; i++) {
+            currentItem = new Item(currentItems[i].title, currentItems[i].desc, currentItems[i].date, null, currentItems[i].id);
+            pushItem(currentItem);
+        }
+    }
+}
+
+function reset() {
+    if (confirm("Are you sure you want to reset?")) {
+        saveSlot = {};
+        currentItems = {};
+        localStorage.setItem("saveSlot", JSON.stringify(saveSlot));
+        localStorage.setItem("itemsList", JSON.stringify(currentItems));
+        location.reload();
+    }
+}
